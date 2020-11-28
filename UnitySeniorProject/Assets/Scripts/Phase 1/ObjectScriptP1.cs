a9253b02f3c6c416fa29b497308de61cd678c886
Adrian Del Bosque <adrianmbm8@gmail.com>
2020-11-27 23:06:39 -0600

    Rearranged file system

---
+++ UnitySeniorProject/Assets/Scripts/Phase 1/ObjectScriptP1.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScriptP1 : MonoBehaviour
+{
+    public AgentScriptP1 agent;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Agent"))
+        {
+            agent.HitObject();
+        }
+    }
+}
---
+++ UnitySeniorProject/Assets/Scripts/Phase 2/ObjectScriptP2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScriptP2 : MonoBehaviour
+{
+    public AgentScriptP2 agent;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Agent"))
+        {
+            agent.HitObject();
+        }
+    }
+}
